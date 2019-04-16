78cb4d3f614fc77f6bb6e26864d17bcc82bf9da1
randomClamshell <khalid@randomclamshell.io>
2019-04-16 09:08:36 -0400

    javascript and styles added

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-//using the javascript date class, we get the current time and date on the visitors computer
+console.log("loaded");
+//using the javascript date class, we get the current time and date on the visitor's computer
 
+
+function updateClock() {
 var currentTime = new Date();
 
-var hours = currentTime.getHours();
-var minutes = currentTime.getMinutes();
-var seconds = currentTime.getSeconds();
+var currentHours = currentTime.getHours();
+var currentMinutes = currentTime.getMinutes();
+var currentSeconds = currentTime.getSeconds();
 
-console.log(currentTime);
+//formatting the time to a string: HH:MM:SS XX, where XX is either “AM” or “PM” using a ternary operator here ? for true : for false
+
+currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+var amOrPm = (currentHours < 12) ? "AM" : "PM";
+currentHours = (currentHours > 12) ? hours - 12 : currentHours;
+currentHours = (currentHours === 0) ? 12 : currentHours;
+
+
+var time = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + amOrPm;
+
+$("#clock").html(time);
+}
+
