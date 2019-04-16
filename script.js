9db49c924f4a0a34e5653bed595cce07d4ab0d80
randomClamshell <khalid@randomclamshell.io>
2019-04-16 09:40:53 -0400

    removed comments

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 console.log("loaded");
-//using the javascript date class, we get the current time and date on the visitor's computer
+
+     function updateClock() {
+          var currentTime = new Date();
+
+          var currentHours = currentTime.getHours();
+          var currentMinutes = currentTime.getMinutes();
+          var currentSeconds = currentTime.getSeconds();
+
+          currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+          currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+          var amOrPm = (currentHours < 12) ? "AM" : "PM";
+          currentHours = (currentHours > 12) ? hours - 12 : currentHours;
+          currentHours = (currentHours === 0) ? 12 : currentHours;
 
 
-function updateClock() {
-var currentTime = new Date();
+          var time = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + amOrPm;
 
-var currentHours = currentTime.getHours();
-var currentMinutes = currentTime.getMinutes();
-var currentSeconds = currentTime.getSeconds();
-
-//formatting the time to a string: HH:MM:SS XX, where XX is either “AM” or “PM” using a ternary operator here ? for true : for false
-
-currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
-
-var amOrPm = (currentHours < 12) ? "AM" : "PM";
-currentHours = (currentHours > 12) ? hours - 12 : currentHours;
-currentHours = (currentHours === 0) ? 12 : currentHours;
-
-
-var time = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + amOrPm;
-
-$("#clock").html(time);
-}
+          $("#clock").html(time);
+     }
 
